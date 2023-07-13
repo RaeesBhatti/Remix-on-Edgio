10e5c4578e8f8a427925ad656a57edd68545c83b
Raees Iqbal <10067728+RaeesBhatti@users.noreply.github.com>
2023-07-13 16:26:21 -0400

    Add Edgio config

---
+++ app.js
@@ -0,0 +1,25 @@
+const { createRequestHandler } = require("@remix-run/express");
+const express = require("express");
+
+const app = express();
+
+// needs to handle all verbs (GET, POST, etc.)
+app.all(
+  "*",
+  createRequestHandler({
+    // `remix build` and `remix dev` output files to a build directory, you need
+    // to pass that build to the request handler
+    build: require("./index"),
+
+    // return anything you want here to be available as `context` in your
+    // loaders and actions. This is where you can bridge the gap between Remix
+    // and your server
+    getLoadContext(req, res) {
+      return {};
+    }
+  })
+);
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
---
+++ routes.ts
@@ -0,0 +1,8 @@
+// This file was automatically added by edgio init.
+// You should commit this file to source control.
+import { Router } from '@edgio/core/router'
+import { customRoutes } from '@edgio/custom-connector'
+
+export default new Router()
+  .use(customRoutes)
+  .static("public")
